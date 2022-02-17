f14e322f787d2b8b07907c7aaa91249ff9e82954
WiktorW-PL <modusii.90@wp.pl>
2022-02-17 20:17:36 +0100

    Add working navigation with some animations. Subpages are Now subpages are available to visit by user.

--- main.js
+++ main.js
@@ -1,8 +1,61 @@
-import Highway from '@dogstudio/highway';
-import Fade from './js/transiton'
+const tl = gsap.timeline();
+const contentTools = document.querySelector('.content');
+const contentGames = document.querySelector('.content-games');
+const contentInfo = document.querySelector('.content-info');
+const btnToGames = document.querySelector('#games-button');
+const btnToTools = document.querySelector('#tools-button');
+const btnToInfo = document.querySelector('#info-button');
 
-const H = new Highway.Core({
-    transitions:{
-        default: Fade
+const loadAnimation = () => {
+    tl.fromTo(contentTools, {opacity: 0}, {opacity: 1, duration: 0.5});
+    contentGames.style.display = "none";
+    contentInfo.style.display = "none";
+}
+loadAnimation()
+
+let currentView = "tools";
+const navigateToGames = () =>{
+    btnToGames.classList.add('nav-list__element--active');
+    if(currentView === "tools"){
+        btnToTools.classList.remove('nav-list__element--active');
+        tl.fromTo(contentTools, {opacity: 1}, {opacity: 0, display: "none", duration: 0.5});
     }
-});
+    else if(currentView === "info"){
+        btnToInfo.classList.remove('nav-list__element--active');
+        tl.fromTo(contentInfo, {opacity: 1}, {opacity: 0, display: "none", duration: 0.5});
+    }
+    tl.fromTo(contentGames, {opacity: 0}, {opacity: 1, display: "flex", duration: 0.5});
+    currentView = "games";
+}
+
+const navigateToTools = () =>{
+    btnToTools.classList.add('nav-list__element--active');
+    if(currentView === "games"){
+        btnToGames.classList.remove('nav-list__element--active');
+        tl.fromTo(contentGames, {opacity: 1}, {opacity: 0, display: "none", duration: 0.5});
+    }
+    else if(currentView === "info"){
+        btnToInfo.classList.remove('nav-list__element--active');
+        tl.fromTo(contentInfo, {opacity: 1}, {opacity: 0, display: "none", duration: 0.5});
+    }
+    tl.fromTo(contentTools, {opacity: 0}, {opacity: 1, display: "flex", duration: 0.5});
+    currentView = "tools";
+}
+
+const navigateToInfo = () =>{
+    btnToInfo.classList.add('nav-list__element--active');
+    if(currentView === "games"){
+        btnToGames.classList.remove('nav-list__element--active');
+        tl.fromTo(contentGames, {opacity: 1}, {opacity: 0, display: "none", duration: 0.5});
+    }
+    else if(currentView === "tools"){
+        btnToTools.classList.remove('nav-list__element--active');
+        tl.fromTo(contentTools, {opacity: 1}, {opacity: 0, display: "none", duration: 0.5});
+    }
+    tl.fromTo(contentInfo, {opacity: 0}, {opacity: 1, display: "flex", duration: 0.5});
+    currentView = "info";
+}
+
+btnToGames.addEventListener('click', navigateToGames);
+btnToTools.addEventListener('click', navigateToTools);
+btnToInfo.addEventListener('click', navigateToInfo);
