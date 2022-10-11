c9f649644d751eecb4cc22c23e166e784220fb06
WiktorW-PL <modusii.90@wp.pl>
2022-10-11 12:03:50 +0200

    Fixes for cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   e2e: {
-    // baseUrl: 'https://admiring-aryabhata-bb43d5.netlify.app/'
-    baseUrl: 'http://127.0.0.1:5500/'
+    baseUrl: 'https://admiring-aryabhata-bb43d5.netlify.app/'
   }
 };
